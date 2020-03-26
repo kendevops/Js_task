985a8cda2c649d281fa9af4853fd06edeb6969ba
kendevops <kendevops@gmail.com>
2020-03-26 18:53:20 +0100

    Coded my assignment

---
+++ app.js
@@ -0,0 +1,19 @@
+//Assign my name to a variable
+let myName = "Kenneth Aladi";
+
+//Assign my courses to an array
+let myCourses = ["Html", "CSS", "JavaScript", "Nodejs", "Go", "Ruby"];
+
+
+//Display My name in Console
+console.log(myName);
+
+//Display My name in Console
+console.log(myCourses);
+
+//Display even numbers 1 to 200 based on my course is even
+for(let i = 1; i <= 200; i++) {
+    if(i%2 === 0) {
+        console.log(i);
+    }
+}
